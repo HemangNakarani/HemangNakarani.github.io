b7f2f7809ad96b1155c331cddc2c4e5d7dc052d4
Hemang Nakarani <hemang.nakarani@sprinklr.com>
2024-04-30 09:08:31 +0530

    new web

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import markdownConfig from "./markdown.config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+import robotsTxt from "astro-robots-txt";
+
+import playformCompress from "@playform/compress";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://hemangnakarani.github.io/",
+  prefetch: true,
+  markdown: {
+    ...markdownConfig,
+  },
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    mdx({
+      ...markdownConfig,
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "one-dark-pro",
+      },
+    }),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    robotsTxt({
+      sitemap: [
+        "https://hemangnakarani.github.io/sitemap-0.xml",
+        "https://hemangnakarani.github.io/sitemap-index.xml",
+      ],
+    }),
+    playformCompress(),
+  ],
+});
